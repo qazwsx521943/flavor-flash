59993db41db1188940ae6a8517ad9728e5d43dfb
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-11-18 16:11:50 +0800

    [feat] Update Restaurant model

--- FlavorFlash/Models/Restaurant/Restaurant.swift
+++ FlavorFlash/Models/Restaurant/Restaurant.swift
@@ -9,18 +9,49 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
+// https://developers.google.com/maps/documentation/places/web-service/reference/rest/v1/places?hl=zh-tw#Place
 struct Restaurant: Hashable, Codable {
-    let title: String
-    let description: String
-    let images: [String]
-    let latitude: CGFloat
-    let longitude: CGFloat
+    let name: String
+    let id: String
+    let displayName: LocalizedText
+    let formattedAddress: String?
+    let shortFormattedAddress: String?
+    let addressComponents: [AddressComponent]?
+    let location: Location
+    let rating: CGFloat?
+    let photos: [Photo]?
 
     var featureImage: Image? {
         Image("home-icon")
     }
 
     var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        CLLocationCoordinate2D(
+            latitude: location.latitude,
+            longitude: location.longitude
+        )
+    }
+
+    struct LocalizedText: Codable, Hashable {
+        let text: String
+        let languageCode: String
+    }
+
+    struct AddressComponent: Codable, Hashable {
+        let longText: String
+        let shortText: String
+        let types: [String]
+        let languageCode: String
+    }
+
+    struct Location: Codable, Hashable {
+        let latitude: CGFloat
+        let longitude: CGFloat
+    }
+
+    struct Photo: Codable, Hashable {
+        let name: String
+        let widthPx: Int
+        let heightPx: Int
     }
 }
