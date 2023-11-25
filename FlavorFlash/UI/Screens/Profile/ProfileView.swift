495fb0e90ae9806eec8eac7a823e1cdc2e32d7c9
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-11-25 13:14:45 +0800

    [feat] QRCode get userId

--- FlavorFlash/UI/Screens/Profile/ProfileView.swift
+++ FlavorFlash/UI/Screens/Profile/ProfileView.swift
@@ -12,12 +12,19 @@ struct ProfileView: View {
 	@StateObject private var viewModel = ProfileViewModel()
 	@EnvironmentObject private var navigationModel: NavigationModel
 	@State private var selectedItem: PhotosPickerItem? = nil
+	@State private var showQRCode = false
+	@State private var qrCodeMode: QRCodeMode = .myQRCode
+	@State private var foundedUser: FFUser?
 
+	enum QRCodeMode: Int {
+		case myQRCode = 0
+		case scanQRCode = 1
+	}
 
-    var body: some View {
-        NavigationStack {
-            VStack {
-                ZStack {
+	var body: some View {
+		NavigationStack {
+			VStack {
+				ZStack {
 					if let imageUrl = viewModel.user?.profileImageUrl {
 						AsyncImage(url: URL(string: imageUrl)) { image in
 							image
@@ -32,7 +39,7 @@ struct ProfileView: View {
 						Circle()
 							.frame(width: 100, height: 100)
 					}
-                }
+				}
 				.overlay(alignment: .bottomTrailing) {
 					PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
 						Image(systemName: "pencil.circle.fill")
@@ -51,7 +58,6 @@ struct ProfileView: View {
 							.font(.subheadline)
 							.foregroundStyle(.gray)
 					}
-
 				}
 			}
 			.task {
@@ -64,28 +70,28 @@ struct ProfileView: View {
 				viewModel.saveProfileImage(item: selected)
 			}
 
-            List {
-//                Section("Settings") {
-//                    ForEach(settingConfigs) { config in
-//                        NavigationLink(value: config) {
-//                            Text(config.title)
-//                        }
-//                    }
-//
-//                }
+			List {
+				//                Section("Settings") {
+				//                    ForEach(settingConfigs) { config in
+				//                        NavigationLink(value: config) {
+				//                            Text(config.title)
+				//                        }
+				//                    }
+				//
+				//                }
 				accountConfigurationView
-            }
-            .navigationDestination(for: SettingItem.self) { setting in
-                Text(setting.title)
-            }
-            .navigationDestination(for: AccountConfigs.self) { config in
-                Button(config.title) {
+			}
+			.navigationDestination(for: SettingItem.self) { setting in
+				Text(setting.title)
+			}
+			.navigationDestination(for: AccountConfigs.self) { config in
+				Button(config.title) {
 
-                }
-                .padding()
-            }
-        }
-    }
+				}
+				.padding()
+			}
+		}
+	}
 }
 
 extension ProfileView {
@@ -97,6 +103,90 @@ extension ProfileView {
 				}
 				.foregroundStyle(config.title == "Delete" ? .red : .white)
 			}
+
+			Text("掃描QRCode")
+				.prefixedWithSFSymbol(named: "qrcode", height: 20, tintColor: .white)
+				.onTapGesture {
+					showQRCode = true
+				}
+				.sheet(isPresented: $showQRCode) {
+					VStack(spacing: 50) {
+						Picker("QRCode scanner", selection: $qrCodeMode) {
+							Text("顯示QRCode").tag(QRCodeMode.myQRCode)
+							Text("掃描QRCode").tag(QRCodeMode.scanQRCode)
+						}
+						.pickerStyle(.segmented)
+						.padding(.horizontal, 16)
+
+						if qrCodeMode == .myQRCode {
+							VStack(alignment: .center) {
+								if let qrCodeData = viewModel.qrCodeImageData {
+									Image(uiImage: UIImage(data: qrCodeData)!)
+								}
+							}
+						} else {
+							if
+								let searchedUser = foundedUser,
+								let profileImageUrl = searchedUser.profileImageUrl
+							{
+								HStack {
+									AsyncImage(url: URL(string: profileImageUrl)) { phase in
+										switch phase {
+										case .success(let image):
+											image
+												.resizable()
+												.scaledToFit()
+												.frame(width: 100, height: 100)
+										case .failure(_):
+											Image(systemName: "person.fill")
+												.resizable()
+												.frame(width: 100, height: 100)
+										case .empty:
+											Image(systemName: "person.fill")
+												.resizable()
+												.frame(width: 100, height: 100)
+										@unknown default:
+											Image(systemName: "person.fill")
+												.resizable()
+												.frame(width: 100, height: 100)
+										}
+									}
+
+									Text(searchedUser.displayName)
+
+									Spacer()
+
+									Button {
+										debugPrint("\(searchedUser.displayName) is  added to your friend list")
+										foundedUser = nil
+									} label: {
+										Text("Add")
+											.padding()
+											.foregroundStyle(.white)
+									}
+									.frame(width: 100, height: 50)
+									.background(Color.purple)
+									.clipShape(RoundedRectangle(cornerRadius: 10))
+
+								}
+							} else {
+								CodeScannerView(codeTypes: [.qr]) { response in
+									switch response {
+									case .success(let result):
+										Task {
+											foundedUser = try await viewModel.getUser(userId: result.string)
+										}
+									case .failure(let error):
+										debugPrint(error.localizedDescription)
+									}
+								}
+								.frame(width: 300, height: 300)
+							}
+						}
+					}
+					.padding(.horizontal, 32)
+				}
+
 			Button {
 				Task {
 					do {
@@ -127,5 +217,5 @@ extension ProfileView {
 }
 
 #Preview {
-    ProfileView()
+	ProfileView()
 }
--- FlavorFlash/UI/Screens/Profile/ProfileViewModel.swift
+++ FlavorFlash/UI/Screens/Profile/ProfileViewModel.swift
@@ -6,9 +6,18 @@
 //
 import SwiftUI
 import PhotosUI
+import UIKit
+
 @MainActor
 final class ProfileViewModel: ObservableObject {
-	@Published private(set) var user: FFUser?
+	@Published private(set) var user: FFUser? {
+		didSet {
+			guard let userId = user?.userId else { return }
+			self.qrCodeImageData = generateQRCode(from: userId)
+		}
+	}
+
+	@Published var qrCodeImageData: Data?
 
 	func logOut() throws {
 		try AuthenticationManager.shared.signOut()
@@ -41,6 +50,30 @@ final class ProfileViewModel: ObservableObject {
 			let url = try await StorageManager.shared.getUrlForImage(path: path)
 			try await UserManager.shared.updateUserProfileImagePath(userId: user.userId, path: path, url: url.absoluteString)
 			try await loadCurrentUser()
+		}
+	}
+	func generateQRCode(from string: String) -> Data? {
+		let data = string.data(using: String.Encoding.ascii)
+
+		if let filter = CIFilter(name: "CIQRCodeGenerator") {
+			filter.setValue(data, forKey: "inputMessage")
+			let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+			if let output = filter.outputImage?.transformed(by: transform) {
+				debugPrint("output qrcode")
+				return UIImage(ciImage: output).pngData()
+			}
+		}
+
+		return nil
+	}
+
+	func getUser(userId: String) async throws -> FFUser {
+		do {
+			let user = try await UserManager.shared.getUser(userId: userId)
+			return user
+		} catch {
+			throw URLError(.badServerResponse)
 		}
 	}
 }
