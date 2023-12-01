46cbe3ba21931642b76744b7e16cf8d6c000c574
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-12-01 14:52:29 +0800

    [refactor] Refactor Authentication & Email signIn UI code

---
+++ FlavorFlash/UI/Components/ViewModifiers/BackgroundModifiers.swift
@@ -0,0 +1,43 @@
+//
+//  BackgroundModifiers.swift
+//  FlavorFlash
+//
+//  Created by 鍾哲玄 on 2023/12/1.
+//
+
+import SwiftUI
+
+struct RoundedRectangleBackgroundModifier: ViewModifier {
+
+	let cornerRadius: CGFloat
+
+	let gradient: Gradient
+
+	func body(content: Content) -> some View {
+		content
+			.background(
+				RoundedRectangle(cornerRadius: 10)
+					.frame(height: 55)
+					.foregroundStyle(
+						LinearGradient(
+							gradient: gradient,
+							startPoint: .topLeading,
+							endPoint: .bottomTrailing)
+					)
+			)
+	}
+
+	init(cornerRadius: CGFloat, gradient: [Color]) {
+		self.cornerRadius = cornerRadius
+		self.gradient = Gradient(colors: gradient)
+	}
+}
+
+extension View {
+	func withRoundedRectangleBackground(cornerRadius: CGFloat = 10.0, gradient: [Color] = [Color.red, Color.purple]) -> some View {
+		modifier(
+			RoundedRectangleBackgroundModifier(
+				cornerRadius: cornerRadius,
+				gradient: gradient))
+	}
+}
--- FlavorFlash/UI/Screens/Authentication/AuthenticationView.swift
+++ FlavorFlash/UI/Screens/Authentication/AuthenticationView.swift
@@ -13,35 +13,85 @@ struct AuthenticationView: View {
 	@EnvironmentObject var navigationModel: NavigationModel
 
 	var body: some View {
-		VStack {
-			NavigationLink {
-				EmailSignInView()
-			} label: {
-				Text("Sign in with Email")
-					.font(.headline)
-					.background(.black)
-					.foregroundStyle(.white)
-					.frame(height: 100)
-					.frame(minWidth: 200)
-					.cornerRadius(10)
-			}
+		VStack(spacing: 10) {
 
-			Button {
-				Task {
-					do {
-						try await authenticationViewModel.signInWithApple()
-						navigationModel.showSignInModal = false
-					} catch {
-						print(error)
-					}
-				}
-			} label: {
-				SignInWithAppleButtonView(type: .default, style: .black)
+			signInWithAppleButton
+
+			signInDivider
+
+			signInWithEmailButton
+
+			HStack {
+				Spacer()
+
+				registerLink
 			}
-			.frame(height: 55)
 		}
-		.padding()
+		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+		.padding(40)
+		.padding(.bottom, 50)
 		.navigationTitle("Sign In")
+		.toolbar(.hidden, for: .navigationBar)
+	}
+}
+
+extension AuthenticationView {
+	// MARK: - Layout
+	private var signInWithAppleButton: some View {
+		Button {
+			Task {
+				do {
+					try await authenticationViewModel.signInWithApple()
+					navigationModel.showSignInModal = false
+				} catch {
+					print(error)
+				}
+			}
+		} label: {
+			SignInWithAppleButtonView(type: .default, style: .white)
+		}
+		.frame(height: 55)
+	}
+
+	private var signInWithEmailButton: some View {
+		NavigationLink {
+			EmailSignInView(state: .signIn)
+		} label: {
+			emailSignInLabel
+		}
+	}
+
+	private var emailSignInLabel: some View {
+		Label("Sign in with Email", systemImage: "envelope.fill")
+			.font(.system(size: 20))
+			.foregroundStyle(.white)
+			.frame(maxWidth: .infinity, maxHeight: 55)
+			.withRoundedRectangleBackground()
+	}
+
+	private var signInDivider: some View {
+		Divider()
+			.frame(height: 2)
+			.overlay(.gray)
+			.padding()
+			.overlay {
+				Text("Continue with Email")
+					.font(.caption)
+					.padding(.horizontal, 10)
+					.background(.black)
+			}
+	}
+
+	private var registerLink: some View {
+		NavigationLink {
+			EmailSignInView(state: .signUp)
+		} label: {
+			Text("Doesn't have an account? Register")
+				.font(.caption)
+				.foregroundStyle(.white)
+				.bold()
+				.underline()
+		}
 	}
 }
 
