a66332265fcb9c1840b314cd3eb4661514671300
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-12-02 16:29:18 +0800

    [refactor] AnalyzeView UI layout

--- FlavorFlash/UI/Screens/FlavorFlash/CameraView/AnalyzeView.swift
+++ FlavorFlash/UI/Screens/FlavorFlash/CameraView/AnalyzeView.swift
@@ -16,40 +16,38 @@ struct AnalyzeView: View {
 
 	var body: some View {
 		GeometryReader { geometry in
-			VStack {
-				if
-					let backCamImage = cameraDataModel.backCamImage,
-					let frontCamImage = cameraDataModel.frontCamImage {
+			if
+				let backCamImage = cameraDataModel.backCamImage,
+				let frontCamImage = cameraDataModel.frontCamImage {
 
-					ZStack {
-						PrimaryPreviewView(previewImage: isBackImagePrimary ? backCamImage : frontCamImage)
-							.frame(width: geometry.size.width, height: geometry.size.height - 80)
-					}
-					.overlay(alignment: .topLeading) {
-						SecondaryPreviewView(previewImage: isBackImagePrimary ? frontCamImage : backCamImage)
-							.onTapGesture {
-								isBackImagePrimary.toggle()
-							}
-					}
-					.onAppear(perform: {
-						debugPrint("UIDevice.current.orientation \(UIDevice.current.orientation)")
-						cameraDataModel.analyzeFood()
-						cameraDataModel.camera.stop()
-					})
-
-					HStack(alignment: .bottom) {
-						NavigationLink {
-							FlavorFlashCommentView(cameraDataModel: cameraDataModel)
-						} label: {
-							Text("Continue ->")
-								.font(.title3)
-								.foregroundStyle(Color.black)
-								.padding()
+				ZStack {
+					PrimaryPreviewView(previewImage: isBackImagePrimary ? backCamImage : frontCamImage)
+						.frame(width: geometry.size.width, height: geometry.size.height - 80)
+				}
+				.overlay(alignment: .topLeading) {
+					SecondaryPreviewView(previewImage: isBackImagePrimary ? frontCamImage : backCamImage)
+						.offset(x: 24, y: 20)
+						.onTapGesture {
+							isBackImagePrimary.toggle()
 						}
-						.background(Color.white)
-						.clipShape(RoundedRectangle(cornerRadius: 20))
+				}
+				.overlay(alignment: .bottomTrailing) {
+					NavigationLink {
+						FlavorFlashCommentView(cameraDataModel: cameraDataModel)
+					} label: {
+						Image(systemName: "arrow.right")
+							.font(.title)
+							.foregroundStyle(Color.black)
+							.padding(24)
+							.background(.white)
+							.clipShape(Circle())
+							.shadow(radius: 10)
 					}
 				}
+				.onAppear(perform: {
+					cameraDataModel.analyzeFood()
+					cameraDataModel.camera.stop()
+				})
 			}
 		}
 	}
