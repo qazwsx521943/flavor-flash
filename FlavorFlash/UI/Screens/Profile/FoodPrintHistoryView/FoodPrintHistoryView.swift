a74bf05182cefaebe1eabad89488a539ed4b0426
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-12-05 20:39:14 +0800

    [feat] Undone: Adding friends foodprint annotation

--- FlavorFlash/UI/Screens/Profile/FoodPrintHistoryView/FoodPrintHistoryView.swift
+++ FlavorFlash/UI/Screens/Profile/FoodPrintHistoryView/FoodPrintHistoryView.swift
@@ -8,16 +8,87 @@
 import SwiftUI
 
 struct FoodPrintHistoryView: View {
-	
+
 	@ObservedObject var profileViewModel: ProfileViewModel
 
-    var body: some View {
-		FoodPrintMapView(profileViewModel: profileViewModel)
-			.navigationTitle("My FoodPrints")
-			.navigationBarTitleDisplayMode(.inline)
-			.toolbar(.hidden, for: .tabBar)
-    }
+	@State private var showFriendSelectionView = false
+
+	var body: some View {
+		ZStack(alignment: .topLeading) {
+			FoodPrintMapView(profileViewModel: profileViewModel)
+
+			Image(systemName: "list.bullet")
+				.resizable()
+				.frame(width: 20, height: 20)
+				.frame(width: 50, height: 50)
+				.background(
+					Circle()
+						.fill(.black.opacity(0.4))
+				)
+				.padding(.leading, 16)
+				.padding(.top, 16)
+				.onTapGesture {
+					showFriendSelectionView.toggle()
+				}
+
+			GeometryReader { geo in
+				VStack {
+					Text("選擇好友足跡")
+						.font(.title2)
+						.bold()
+						.padding(.top, 16)
+					List {
+						ForEach(profileViewModel.friends) { friend in
+							Text(friend.displayName)
+								.font(.title3)
+								.bold()
+								.tint(.white)
+								.onTapGesture {
+									Task {
+										try await profileViewModel.getFriendFoodPrint(userId: friend.id)
+									}
+								}
+						}
+					}
+					.listStyle(.plain)
+				}
+				.frame(maxWidth: .infinity, maxHeight: .infinity)
+				.background(.black)
+				.frame(
+					width: geo.size.width,
+					height: geo.size.height / 2
+				)
+				.offset(y: showFriendSelectionView ? geo.size.height / 2 : geo.size.height)
+				.animation(.easeIn, value: showFriendSelectionView)
+				.transition(.move(edge: .bottom))
+			}
+		}
+		.ignoresSafeArea(edges: .bottom)
+		.toolbar(.hidden, for: .tabBar)
+		.navigationTitle("My FoodPrints")
+		.navigationBarTitleDisplayMode(.inline)
+	}
 }
+
+//struct FriendFoodPrintSelectionView: View {
+//
+//	@Binding var friendList: [FFUser]
+//
+//	var body: some View {
+//		VStack {
+//			List {
+//				ForEach(friendList) { friend in
+//					Text(friend.displayName)
+//						.font(.title3)
+//						.bold()
+//						.tint(.white)
+//				}
+//			}
+//		}
+//		.frame(maxWidth: .infinity, maxHeight: .infinity)
+//		.background(.black.opacity(0.4))
+//	}
+//}
 
 //#Preview {
 //    FoodPrintHistoryView()
--- FlavorFlash/UI/Screens/Profile/FoodPrintHistoryView/FoodPrintMapView.swift
+++ FlavorFlash/UI/Screens/Profile/FoodPrintHistoryView/FoodPrintMapView.swift
@@ -37,6 +37,22 @@ struct FoodPrintMapView: UIViewRepresentable {
 				uiView.addAnnotation(annotation)
 			}
 		}
+
+		debugPrint("friend foodPrints: \(profileViewModel.friendFoodPrints.map { $0.id })")
+		for friendFoodPrint in profileViewModel.friendFoodPrints {
+			if let location = friendFoodPrint.location {
+				let coordinate = CLLocationCoordinate2D(location: location)
+				let annotation = FoodPrintAnnotation(
+					coordinate: coordinate,
+					title: friendFoodPrint.id,
+					subtitle: friendFoodPrint.restaurantId,
+					imageUrl: friendFoodPrint.backCameraImageUrl
+				)
+
+				uiView.addAnnotation(annotation)
+			}
+		}
+
 		if !profileViewModel.foodPrints.isEmpty {
 			uiView.setRegion(MKCoordinateRegion(
 				center: CLLocationCoordinate2D(location: profileViewModel.foodPrints.first!.location!),
