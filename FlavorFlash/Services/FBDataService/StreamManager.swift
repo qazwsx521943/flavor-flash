9f5ae06d2f963adc749f9402875554119bdabfc5
Jason's Macbook <qazwsx5219430630@gmail.com>
2023-12-25 19:45:57 +0800

    [feat] Add LiveStreamView

---
+++ FlavorFlash/Services/FBDataService/StreamManager.swift
@@ -0,0 +1,8 @@
+//
+//  StreamManager.swift
+//  FlavorFlash
+//
+//  Created by 鍾哲玄 on 2023/12/25.
+//
+
+import Foundation
---
+++ FlavorFlash/UI/Screens/LiveStream/LiveStreamView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveStreamView.swift
+//  FlavorFlash
+//
+//  Created by 鍾哲玄 on 2023/12/25.
+//
+
+import SwiftUI
+
+struct LiveStreamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveStreamView()
+}
